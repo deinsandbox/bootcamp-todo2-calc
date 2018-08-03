8891ad2a96e7c2522fc260725c4cebbf342bf8f9
equiman <equiman@users.noreply.github.com>
2018-08-03 11:05:31 -0500

    show result on component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,23 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'calc';
+  first: number = 0;
+  second: number = 0;
+  result: number = 0;
+
+  setFirst(value: number) {
+    this.first = value;
+    this.setResult();
+  }
+
+  setSecond(value: number) {
+    this.second = value;
+    this.setResult();
+  }
+
+  setResult() {
+    console.log(`First ${this.first} Type ${typeof this.first}`);
+    console.log(`Second ${this.second} Type ${typeof this.second}`);
+    this.result = this.first + this.second;
+  }
 }
